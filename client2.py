aad46139abbf774edade64c7db981f81c21b607b
naufalathief <naufalma12@gmail.com>
2021-06-21 03:00:47 +0700

    Update client2.py

--- client2.py
+++ client2.py
@@ -12,31 +12,60 @@ TCP_PORT = 5005
 BUFFER_SIZE = 1024
 
 # Create socket TCP
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-num_1 = 10
-num_2 = 2
+num1 = int(input('Input 1st Number: '))
+num2 = int(input('Input 2nd Number: '))
+print('Calculation Method:')
+print('1. Add')
+print('2. Subtract')
+print('3. Multiply')
+print('4. Divide')
+print('5. Modulo')
+choice = input('Select Calculation Method: ')
 
 
-def add(x, y):
-    print('Addition', x, y)
+
+def addition(x, y):
     return x+y
 
+def subtract(x, y):
+    return x-y
 
-add_result = add(num_1, num_2)
+def multiply(x, y):
+    return x*y
+
+def divide(x, y):
+    return x/y
+
+def modulo(x, y):
+    return x%y
+
+if choice in ('1', '2', '3', '4', '5'):
+    if choice == '1':
+        result = addition(num1, num2)
+    elif choice == '2':
+        result = subtract(num1, num2)
+    elif choice == '3':
+        result = multiply(num1, num2)
+    elif choice == '4':
+        result = divide(num1, num2)
+    elif choce == '5':
+        result = modulo(num1, num2)
+else:
+    print("Invalid Input or Unavailable Calculation")
 
 # Connect to server with IP and Port parameter
 s.connect((TCP_IP, TCP_PORT))
 
-# Send Message To server
-s.send(str(add_result).encode())
+# Send Message to Server
+s.send(str(result).encode())
+
 # Receive Message from server
 data = s.recv(BUFFER_SIZE)
 
 # Display Message from Server
-
-# Display Message from Server
-print("Data Recieved!")
+print("Calculation Complete! Check LogRecord")
 
 # Close Connection
 s.close()
