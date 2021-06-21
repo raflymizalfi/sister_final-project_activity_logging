a9a5b23a0358393f5d48fc509fcaf361d6e0b262
naufalathief <naufalma12@gmail.com>
2021-06-21 22:31:51 +0700

    Update client1.py

--- client1.py
+++ client1.py
@@ -6,7 +6,7 @@ import copy
 TCP_IP = '26.51.151.124'
 
 # Define Port
-TCP_PORT = 5005
+TCP_PORT = 6969
 
 # Define Buffer Size
 BUFFER_SIZE = 1024
@@ -24,15 +24,16 @@ s.connect((TCP_IP, TCP_PORT))
 s.send(Message.encode())
 
 # Show Message from Server
-print("Message:", Message)
-print('Write a Message?')
-print('1. Yes')
-print('2. No')
-print('3. Show Activity Log')
-Choice = input('Choice: ')
 
-while 1:
-    while Choice == '1':
+while True:
+    print("Message:", Message)
+    print('Write a Message?')
+    print('1. Yes')
+    print('2. No')
+    print('3. Show Activity Log')
+    Choice = input('Choice: ')
+
+    if Choice == '1':
         Message = input("Send a Message: ")
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
     # Connect to server with the IP parameter and Port that's defined
@@ -57,25 +58,21 @@ while 1:
         f = open('LogRecord.log', 'r')
         file_contents = f.read()
         print(file_contents)
-        print("1. Delete Contents")
-        print("2. Copy File")
-        print("3. Return")
         print("What will you do now?")
+        print("1. Delete Contents")
+        print("2. Return")
         nextChoice = input("Choice: ")
         if nextChoice == '1':
+            f = open('LogRecord.log', 'w')
             f.truncate()
             print("Content has been deleted. Check your file.")
+            print("1. Delete Contents")
+            print("2. Return")
+            nextChoice = input("Choice: ")
         if nextChoice == '2':
-            copy.deepcopy(f)
-            print("Content has been copied. Check your folder")
-        if nextChoice == '3':
-            print('Write a message?')
-            print('1. Yes')
-            print('2. No')
-            print('3. Show Activity Log')
-            Choice = input('Choice: ')
+            pass
         else:
-            done = input('Done? (Y/N) :')
+            done = input('No choices available. Would you like to proceed? (Y/N): ')
             if done == 'Y' or done == 'y':
                 f.close()
                 print('Write a message?')
@@ -84,12 +81,16 @@ while 1:
                 print('3. Show Activity Log')
                 Choice = input('Choice: ')
             if done == 'N' or done == 'n':
-                nextChoice = input("What will you do now?")
+                print("What will you do now?")
                 print("1. Delete Contents")
-                print("2. Copy File")
-                print("3. Return")
+                print("2. Return")
+                nextChoice = input('Choice: ')
+            else:
+                print("No Choices Available")
+                print("1. Delete Contents")
+                print("2. Return")
+                nextChoice = input('Choice: ')
     else:
         print("No choices available, select another choice.")
         Choice = input('Choice: ')
-    s.close()
-    break
+s.close()
