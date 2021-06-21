bb1fddce3b5c7a6e03d200e0f158e08f2d3057a3
naufalathief <naufalma12@gmail.com>
2021-06-21 23:07:24 +0700

    Update client1.py

--- client1.py
+++ client1.py
@@ -1,6 +1,6 @@
 # Importing Socket
 import socket
-import copy
+
 
 # Define IP Address
 TCP_IP = '26.51.151.124'
@@ -11,7 +11,7 @@ TCP_PORT = 6969
 # Define Buffer Size
 BUFFER_SIZE = 1024
 
-# Define Message
+# Define Sender and Message
 Message = input("Send a Message: ")
 
 # Create TCP Socket
@@ -24,25 +24,19 @@ s.connect((TCP_IP, TCP_PORT))
 s.send(Message.encode())
 
 # Show Message from Server
+print("Message:", Message)
+print('Write a Message?')
+print('1. Yes')
+print('2. No')
+print('3. Show Activity Log')
+Choice = input('Choice: ')
 
 while True:
-    print("Message:", Message)
-    print('Write a Message?')
-    print('1. Yes')
-    print('2. No')
-    print('3. Show Activity Log')
-    Choice = input('Choice: ')
-
     if Choice == '1':
         Message = input("Send a Message: ")
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
-    # Connect to server with the IP parameter and Port that's defined
         s.connect((TCP_IP, TCP_PORT))
-    # Send message to server
         s.send(Message.encode())
-    # Receive message from server
-        data = s.recv(BUFFER_SIZE)
-    # Display Messages from Server
         print("Message:", Message)
         print('Write a message?')
         print('1. Yes')
@@ -54,7 +48,6 @@ while True:
         print('Thank you for your attention!')
         s.close()
         break
-    # test
     if Choice == '3':
         f = open('LogRecord.log', 'r')
         file_contents = f.read()
@@ -71,11 +64,15 @@ while True:
             print("2. Return")
             nextChoice = input("Choice: ")
         if nextChoice == '2':
-            pass
+            f.close()
+            print('Write a message?')
+            print('1. Yes')
+            print('2. No')
+            print('3. Show Activity Log')
+            Choice = input('Choice: ')
         else:
             done = input('No choices available. Would you like to proceed? (Y/N): ')
             if done == 'Y' or done == 'y':
-                f.close()
                 print('Write a message?')
                 print('1. Yes')
                 print('2. No')
@@ -93,5 +90,8 @@ while True:
                 nextChoice = input('Choice: ')
     else:
         print("No choices available, select another choice.")
+        print('1. Yes')
+        print('2. No')
+        print('3. Show Activity Log')
         Choice = input('Choice: ')
 s.close()
